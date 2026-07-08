d500e9c6a78b9750d089c7f422fbd380d22e988d
Juan villada <villadaurechejuandavid20@gmail.com>
2026-07-08 06:56:12 -0500

    fix: eliminar import inexistente de publicRateLimit en menu.routes

--- backend/src/routes/menu.routes.js
+++ backend/src/routes/menu.routes.js
@@ -1,8 +1,7 @@
 import { Router } from 'express';
 import * as menuController from '../controllers/menu.controller.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
-import { publicRateLimit } from '../middlewares/rateLimit.middleware.js';
 
 export const menuRouter = Router();
 
-menuRouter.get('/', publicRateLimit, asyncHandler(menuController.getMenu));
+menuRouter.get('/', asyncHandler(menuController.getMenu));
